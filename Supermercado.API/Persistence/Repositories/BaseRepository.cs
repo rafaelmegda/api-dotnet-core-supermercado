9857922af0bbfc7b31ab5b092e4d7f504a2eb812
rafaelmegda <rafaelmisidoro@gmail.com>
2021-09-01 16:37:11 -0300

    Add Repositorys

---
+++ Supermercado.API/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,11 @@
+namespace Supermercado.API.Persistence.Repositories
+{
+    public class BaseRepository
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context){
+            _context = context;
+        }
+    }
+}
